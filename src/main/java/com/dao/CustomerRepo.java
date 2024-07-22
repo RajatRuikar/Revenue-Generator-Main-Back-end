0ba92e6b9e662f0e77bb2a89ad920ab9e452cc09
RajatRuikar <rajatruikar12@gmail.com>
2024-07-22 21:54:50 +0530

    Changes

--- src/main/java/com/dao/CustomerRepo.java
+++ src/main/java/com/dao/CustomerRepo.java
@@ -1,7 +1,7 @@
 package com.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.Query;
 
 import com.model.Customer;
 
